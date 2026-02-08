34d46f3e2d643d8778abf98d4c84c7ee16c8cb60
Anton Huggard <antonhuggard@gmail.com>
2026-02-08 13:40:22 +1300

    + about page

--- webpages/views.py
+++ webpages/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def about(request):
     context = {"content": "Je ne sais pas"}
-    return render(request, "webpages/index.html", context)
+    return render(request, "webpages/about.html", context)
